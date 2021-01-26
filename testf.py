e46d948536f58ed59b1caf589379ce1de1b1418d
Artur <70091602+piraartur@users.noreply.github.com>
2021-01-26 10:39:14 +0100

    Add files via upload
    Fixed while loop that ensures app runs after user manipulation.
    FIXED:
    After choosing second operation (calculate volatility) app somehow didn't recognise first time input and skipped the if statement, but on 2nd and n-th cycle after same input it ran smoothly.

--- testf.py
+++ testf.py
@@ -34,44 +34,45 @@ while answer != "YES":
 vlt.decoration()
 
 answer =  vlt.menu()
-while answer != 0:
-	if answer == 1:
-		print("Please type name of your chart, or leave it blank.")
-		name_of_asset = input()
-		print("Please state in which column there are values for price highs BY ENTEING NUMBER.")
-		a = int(input())
-		print("Please state in which column there are date values BY ENTERING NUMBER.")
-		b = int(input())
-		dates, highs, mean = vlt.analyze_csv(filename,a,b)
-		fig, ax = mp.subplots()
-		ax.plot(dates, highs, c='black')
-		ax.set_title(f" {name_of_asset} ", fontsize=14)
-		ax.set_ylabel("Price")
-		ax.set_xlabel("Date")
-		fig.autofmt_xdate()
-		ax.tick_params(axis='both', which='major', labelsize=14)
-		mp.show()
-		vlt.decoration()
-		answer = 3
-	answer=vlt.menu()
-	
-	if answer == 2:
-		print("Please state in which column there are values for price highs BY ENTERING NUMBER.")
-		a = int(input())
-		dates, highs, mean = vlt.analyze_csv(filename,a)
-		crrt_price = highs[-1]
-		variances = [] 
-		for high in highs:
-			dffr = mean-high #dffr - difference
-			sqrd_dffr = dffr*dffr
-			variances.append(sqrd_dffr)
-		mean_variance = sum(variances)/len(variances)
-		volatility = math.sqrt(mean_variance)
-		daily_volatility = volatility/math.sqrt(252)
-		vlt.decoration()
-		print(f"Calcuated value of annualized volatility equals {volatility}.")
-		print(f"Calculated value of daily volatility equals {daily_volatility}.")
-		vlt.decoration()
-		answer = 3
-	answer = vlt.menu()
+if answer != 6:
+	while answer > 0:
+		if answer == 1:
+			print("Please type name of your chart, or leave it blank.")
+			name_of_asset = input()
+			print("Please state in which column there are values for price highs BY ENTEING NUMBER.")
+			a = int(input())
+			print("Please state in which column there are date values BY ENTERING NUMBER.")
+			b = int(input())
+			dates, highs, mean = vlt.analyze_csv(filename,a,b)
+			fig, ax = mp.subplots()
+			ax.plot(dates, highs, c='black')
+			ax.set_title(f" {name_of_asset} ", fontsize=14)
+			ax.set_ylabel("Price")
+			ax.set_xlabel("Date")
+			fig.autofmt_xdate()
+			ax.tick_params(axis='both', which='major', labelsize=14)
+			mp.show()
+			vlt.decoration()
+			answer = 0
+		answer=vlt.menu()
+
+		if answer == 2:
+			print("Please state in which column there are values for price highs BY ENTERING NUMBER.")
+			a = int(input())
+			dates, highs, mean = vlt.analyze_csv(filename,a)
+			crrt_price = highs[-1]
+			variances = [] 
+			for high in highs:
+				dffr = mean-high #dffr - difference
+				sqrd_dffr = dffr*dffr
+				variances.append(sqrd_dffr)
+			mean_variance = sum(variances)/len(variances)
+			volatility = math.sqrt(mean_variance)
+			daily_volatility = volatility/math.sqrt(252)
+			vlt.decoration()
+			print(f"Calcuated value of annualized volatility equals {volatility}.")
+			print(f"Calculated value of daily volatility equals {daily_volatility}.")
+			vlt.decoration()
+			answer = 0
+			answer = vlt.menu()
 		
